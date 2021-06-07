fe8f016e8d259280c5ee8c53d93a4a8b9f89af72
sakurapath <leandro.canestraro050@gmail.com>
2021-06-07 15:27:08 -0300

    getting req and res

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,6 +1,9 @@
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('voce esta na rota de atendimentos e esta realizando um get'))
 
-    app.post('atendimentos', (req, res) => res.send('voce esta na rota de atendimentos e esta realizando um post'))
+    app.post('/atendimentos', (req, res) => {
+        console.log(req.body)
+        res.send('voce esta na rota de atendimentos e esta realizando um post')
+    })
 }
 
