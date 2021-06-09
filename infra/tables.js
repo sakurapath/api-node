9a8ba8e801533f3eee499f3013961c054a07b0c8
sakurapath <leandro.canestraro050@gmail.com>
2021-06-09 14:41:43 -0300

    some erros still persist

--- infra/tables.js
+++ infra/tables.js
@@ -6,7 +6,8 @@ class Tables {
     }
 
     criarAtendimentos() {
-        const query = 'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, status varchar(20) NOT NULL, observ text, PRIMARY KEY(id))'
+
+        const query = 'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, data datetime NOT NULL, dataCriacao datetime NOT NULL, cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, status varchar(20) NOT NULL, observ text, PRIMARY KEY(id))'
 
         this.connection.query(query, (erro) => {
             if (erro) {
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,7 +1,11 @@
+const moment = require('moment')
 const connection = require('../infra/connection')
 
 class Atendimento {
     adiciona(atendimento) {
+        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+        const atendimentoDatado = { ...atendimento, dataCriacao, data }
         const query = 'INSERT INTO Atendimentos SET ?'
         connection.query(query, atendimento, (erro, resultados) => {
             if (erro) {
